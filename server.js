8c9b3cb201d3e0ab9604b0e51dd8aa1dd4a17dae
parikshitexe <parikshitsharma704@gmail.com>
2026-05-23 17:21:27 +0530

    feat: initialize server with Express, Socket.IO, and YSocket.IO; add .gitignore and package.json

---
+++ server.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import { YSocketIO } from 'y-socket.io/dist/server';
+import { createServer} from 'http';
+import { Server } from 'socket.io';
+
+const app = express();
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST']
+    }}
+);
+
+const ySocketIO = new YSocketIO(io);
+ySocketIO.initialize();
+
+app.get('/', (req, res)=>{
+    res.status(200).json({
+        message: 'Welcome to the backend server',
+        success: true
+    })
+})
+
+app.get('/health', (req, res)=>{
+    res.status(200).json({
+        message: 'Server is healthy',
+        success: true
+    })
+})
+
+
+httpServer.listen(3000, ()=>{
+    console.log('Server is running on port 3000');
+})
